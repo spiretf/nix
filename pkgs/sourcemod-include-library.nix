9a1881d10afb6d17bcbba6d042044a30cf5705b6
Robin Appelman <robin@icewind.nl>
2023-11-10 23:05:15 +0100

    fmt

--- pkgs/sourcemod-include-library.nix
+++ pkgs/sourcemod-include-library.nix
@@ -1,20 +1,21 @@
 {
   stdenv,
   fetchFromGitHub,
-}: stdenv.mkDerivation rec {
-    pname = "sourcemod-include-library";
-    version = "19-10-2022";
+}:
+stdenv.mkDerivation rec {
+  pname = "sourcemod-include-library";
+  version = "19-10-2022";
 
-    dontBuild = true;
+  dontBuild = true;
 
-    src = fetchFromGitHub {
-      owner = "JoinedSenses";
-      repo = "SourceMod-IncludeLibrary";
-      rev = "9d4e4263b77aa0c4fcdadd498c1420c64b3c1c10";
-      hash = "sha256-kO3iRHr3D0qLmgMh4SD3yz14bxSUD0r9pi6dHj8dnN0=";
-    };
+  src = fetchFromGitHub {
+    owner = "JoinedSenses";
+    repo = "SourceMod-IncludeLibrary";
+    rev = "9d4e4263b77aa0c4fcdadd498c1420c64b3c1c10";
+    hash = "sha256-kO3iRHr3D0qLmgMh4SD3yz14bxSUD0r9pi6dHj8dnN0=";
+  };
 
-    installPhase = ''
-      cp -r $src $out
-    '';
-  }
+  installPhase = ''
+    cp -r $src $out
+  '';
+}
